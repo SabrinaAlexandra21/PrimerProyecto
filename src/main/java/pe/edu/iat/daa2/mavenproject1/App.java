bf1c08c601efc077b3251f4c71291b0f4758e2dc
SabrinaAlexandra21 <57188617+SabrinaAlexandra21@users.noreply.github.com>
2019-10-30 11:36:23 -0500

    Update App.java

--- src/main/java/pe/edu/iat/daa2/mavenproject1/App.java
+++ src/main/java/pe/edu/iat/daa2/mavenproject1/App.java
@@ -1,7 +1,7 @@
 package pe.edu.iat.daa2.mavenproject1;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola mundoxd");
     }
    
 }
