c53e9917ac42238d74a4346f8dc0907250889edd
Tatiana <A18100732@idat.edu.pe>
2019-10-30 11:52:58 -0500

    + Mensaje :)

--- src/main/java/pe/edu/iat/daa2/mavenproject1/App.java
+++ src/main/java/pe/edu/iat/daa2/mavenproject1/App.java
@@ -2,6 +2,7 @@ package pe.edu.iat.daa2.mavenproject1;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hola mundoxd");
+        System.out.println("Ya te jackie");
     }
    
 }
